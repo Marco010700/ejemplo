631e7ad1f06a22bb2f4fde1271e723f5ea6047f5
Marco Antonio Alviter Rodríguez <marco.a.alviter@gmail.com>
2024-10-18 15:07:23 -0600

    funciones del back

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\EmpleadoControlles;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/empleados/{id}', [EmpleadoControlles::class, 'show']);
+Route::post('/empleados', [EmpleadoControlles::class, 'store']);
+Route::put('/empleados/{id}/status', [EmpleadoControlles::class, 'updateStatus']);
+Route::delete('/empleados/{id}', [EmpleadoControlles::class, 'destroy']);
+Route::get('/empleados', [EmpleadoControlles::class, 'index']);
+
+Route::get('/departamentos', [DepartamentoController::class, 'index']);
+Route::get('/departamentos/{id}', [DepartamentoController::class, 'show']);
